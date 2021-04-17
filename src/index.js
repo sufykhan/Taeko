2492b17c638f931b90a52db7aacc076538c64519
Almighty Stranger <sufyankhan9678@gmail.com>
2021-04-17 22:58:36 +0530

    file changes

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import "./bootstrap.min.css"
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from "react-router-dom"
-import store from "./store"
-import {Provider} from "react-redux"
 ReactDOM.render(
-  <Provider store={store}>
+
   <Router>
   <App />
-  </Router>
-  </Provider>,
+  </Router>,
   document.getElementById('root')
 );
